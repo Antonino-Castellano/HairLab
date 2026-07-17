ed6e1f9e8720d0652996d007daa4ac9b5146a693
Antonino <antoninocastellano06@gmail.com>
2026-07-17 17:52:34 +0200

    First Commit. Commit per creazione progetto Git da caricare su GitHub

---
+++ src/main/java/com/generation/hairlab/HairlabApplication.java
@@ -0,0 +1,13 @@
+package com.generation.hairlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/hairlab/HairlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.hairlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
