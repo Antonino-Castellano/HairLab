eabb333ccf4278545ebaec951507f05d3a5f989b
Antonino <antoninocastellano06@gmail.com>
2026-07-20 09:49:47 +0200

    Terzo commit. Creato la classe Appointment

--- src/main/java/com/generation/hairlab/model/Appointment.java
+++ src/main/java/com/generation/hairlab/model/Appointment.java
@@ -1,5 +1,52 @@
 package com.generation.hairlab.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
 public class Appointment {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Column(nullable = false)
+    private LocalDateTime startDateTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AppointmentStatus status;
+
+    private String notes;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
 }
+
+
+/*
+-ID
+-CUSTOMER_ID
+-START_DATETIME
+-STATUS
+-NOTES
+-CREATED_AT
+-UPDATED_AT
+*/
